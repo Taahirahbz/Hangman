d2ee72767357a762e4b8eb5036d3a2d74bd316e6
Taahirah <taahirahbz@gmail.com>
2026-01-22 13:34:40 +0400

    Fix formatting of game rules in gamecore.py

--- gamecore.py
+++ gamecore.py
@@ -4,9 +4,7 @@ print ("Wellcome to Taahirah's game. It is just a biology version of the Hangman
 print input("Press enter to continue: ")
 name = (input("Kindly input your name: "))
 print ("Hello " + name + ", let's start the game. Kindly read the game rules below then press enter to continue.")
-print (" The Objective is guess the hidden word by suggesting letters before you run out of chances. The player guesses one letter at a time.
-If the letter is in the word: All matching positions are revealed, else one attempt is lost. The player wins by guessing all letters in the word before attempts run out. 
-The player loses if all allowed wrong guesses are used before the word is complete. All words are 6 letter and you will get only 3 hints")
+print (" The Objective is guess the hidden word by suggesting letters before you run out of chances. The player guesses one letter at a time. If the letter is in the word: All matching positions are revealed, else one attempt is lost. The player wins by guessing all letters in the word before attempts run out. The player loses if all allowed wrong guesses are used before the word is complete. All words are 6 letter and you will get only 3 hints")
 print input()
 
 
