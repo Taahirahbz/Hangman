0a28e0a63cafbf84f12df62f2a162b33f416ed25
Taahirah <taahirahbz@gmail.com>
2026-01-22 13:49:15 +0400

    Update gamecore.py

--- gamecore.py
+++ gamecore.py
@@ -1,14 +1,15 @@
 import random
 
-print ("Wellcome to Taahirah's game. It is just a biology version of the Hangman you know!")
-print input("Press enter to continue: ")
+print ("Welcome to Taahirah's game. It is just a biology version of the Hangman you know!")
+print input("Press enter to continue")
 name = (input("Kindly input your name: "))
 print ("Hello " + name + ", let's start the game. Kindly read the game rules below then press enter to continue.")
-print ("The Objective is guess the hidden word by suggesting letters before you run out of chances. \nThe player guesses one letter at a time.")
-print ("If the letter is in the word, All matching positions are revealed, else one attempt is lost.")
-print ("The player wins by guessing all letters in the word before attempts run out.")
-print ("The player loses if all allowed wrong guesses are used before the word is complete.")
-print ("All words are 6 letter and you will get only 3 hints")
+print ("The Objective is guess the hidden word by suggesting letters before you run out of chances.")
+print ("1. The player guesses one letter at a time.")
+print ("2. If the letter is in the word, All matching positions are revealed, else one attempt is lost.")
+print ("3. The player wins by guessing all letters in the word before attempts run out.")
+print ("4. The player loses if all allowed wrong guesses are used before the word is complete.")
+print ("5. All words are 6 letter and you will get only 3 hints")
 print input()
 
 
