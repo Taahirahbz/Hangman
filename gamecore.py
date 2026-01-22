487eb1fda7e840ea0f7352ed15f3e69ccffc150f
Taahirah <taahirahbz@gmail.com>
2026-01-22 11:33:41 +0400

    Initiation
    Added welcome message and game instructions for Hangman.

--- gamecore.py
+++ gamecore.py
@@ -1,1 +1,14 @@
+print ("Wellcome to Taahirah's Hangman game")
+print input("Press enter to continue: ")
+name = (input("Kindly input your name: "))
+print ("Hello " + name + ", let's start the game. Kindly read the game rules below then press enter to continue.")
+print (" The Objective is guess the hidden word by suggesting letters before you run out of chances. The player guesses one letter at a time.
+If the letter is in the word: All matching positions are revealed, else one attempt is lost. The player wins by guessing all letters in the word before attempts run out. 
+The player loses if all allowed wrong guesses are used before the word is complete")
 
+print input()
+
+
+
+
+       
