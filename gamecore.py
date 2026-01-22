e51d4c5beaa4aebc2cf6d777736efcbef3261007
Taahirah <taahirahbz@gmail.com>
2026-01-22 13:40:44 +0400

    Update gamecore.py

--- gamecore.py
+++ gamecore.py
@@ -4,7 +4,11 @@ print ("Wellcome to Taahirah's game. It is just a biology version of the Hangman
 print input("Press enter to continue: ")
 name = (input("Kindly input your name: "))
 print ("Hello " + name + ", let's start the game. Kindly read the game rules below then press enter to continue.")
-print ("The Objective is guess the hidden word by suggesting letters before you run out of chances. \nThe player guesses one letter at a time. If the letter is in the word, All matching positions are revealed, else one attempt is lost. \nThe player wins by guessing all letters in the word before attempts run out. \nThe player loses if all allowed wrong guesses are used before the word is complete. \nAll words are 6 letter and you will get only 3 hints")
+print ("The Objective is guess the hidden word by suggesting letters before you run out of chances. \nThe player guesses one letter at a time.")
+print ("If the letter is in the word, All matching positions are revealed, else one attempt is lost.")
+print ("The player wins by guessing all letters in the word before attempts run out.")
+print ("The player loses if all allowed wrong guesses are used before the word is complete.")
+print ("All words are 6 letter and you will get only 3 hints")
 print input()
 
 
